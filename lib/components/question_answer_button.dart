e7fe8f69dfe90656535bfcb77eac1f549eb328ac
Melkizedeki <mmagota2@gmail.com>
2025-04-09 21:51:41 +0300

    finishing up with diplaing the answers of the questions looking forward for shruffling

---
+++ lib/components/question_answer_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onSelectAnswer});
+
+  final String answerText;
+  final VoidCallback onSelectAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+          vertical: MediaQuery.of(context).size.height * 0.01),
+      child: ElevatedButton(
+        onPressed: () {
+          onSelectAnswer();
+        },
+        style: ElevatedButton.styleFrom(
+          foregroundColor: const Color.fromARGB(255, 155, 130, 130),
+          backgroundColor: const Color.fromARGB(255, 69, 15, 78),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.1,
+            vertical: MediaQuery.of(context).size.height * 0.02,
+          ),
+        ),
+        child: Text(answerText),
+      ),
+    );
+  }
+}
--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app2/components/question_answer_button.dart';
+import 'package:quiz_app2/data/quiz_quesions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key, required this.backToStartScreen});
@@ -9,66 +11,71 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestion = quizQuestions[3];
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Text(
-            "Questions Screen",
+          Text(
+            currentQuestion.questionText,
+            textAlign: TextAlign.center,
             style: TextStyle(
               color: Colors.white,
               fontSize: 20,
             ),
           ),
-          const SizedBox(
-            height: 20,
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.1,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                ...currentQuestion.answers.map(
+                  (answer) {
+                    return AnswerButton(
+                      answerText: answer,
+                      onSelectAnswer: () {},
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
-          ElevatedButton(
-            onPressed: null,
-            child: Text("Question 1"),
-          ),
-          ElevatedButton(
-            onPressed: null,
-            child: Text("Question 2"),
-          ),
-          ElevatedButton(
-            onPressed: null,
-            child: Text("Question 3"),
-          ),
-          ElevatedButton(
-            onPressed: null,
-            child: Text("Question 4"),
-          ),
-
-          // OutlinedButton.icon(
-          //   style: OutlinedButton.styleFrom(
-          //     foregroundColor: Colors.white,
-          //     padding: const EdgeInsets.symmetric(
-          //       horizontal: 20,
-          //       vertical: 10,
-          //     ),
-          //   ),
-          //   onPressed: () {
-          //     widget.backToStartScreen();
-          //   },
-          //   icon: const Icon(
-          //     Icons.arrow_back,
-          //     color: Colors.white,
-          //   ),
-          //   label: const Text(
-          //     "Back",
-          //     style: TextStyle(
-          //         fontSize: 20,
-          //         fontWeight: FontWeight.bold,
-          //         color: Colors.white),
-          //   ),
-          // ),
         ],
       ),
+
+      // OutlinedButton.icon(
+      //   style: OutlinedButton.styleFrom(
+      //     foregroundColor: Colors.white,
+      //     padding: const EdgeInsets.symmetric(
+      //       horizontal: 20,
+      //       vertical: 10,
+      //     ),
+      //   ),
+      //   onPressed: () {
+      //     widget.backToStartScreen();
+      //   },
+      //   icon: const Icon(
+      //     Icons.arrow_back,
+      //     color: Colors.white,
+      //   ),
+      //   label: const Text(
+      //     "Back",
+      //     style: TextStyle(
+      //         fontSize: 20,
+      //         fontWeight: FontWeight.bold,
+      //         color: Colors.white),
+      //   ),
+      // ),
     );
   }
 }
